516fbf308dbc5b3ac4c93598a792bc606d2307ce
TaekJinJang <93184838+TaekJinJang@users.noreply.github.com>
2022-11-29 17:40:20 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.js
@@ -0,0 +1,13 @@
+function solution(n) {
+    // let pizza = 1
+    // if (n<=7) {
+    //     return pizza;
+    // } else {
+    //     pizza = pizza + n/7
+    //     return Math.floor(pizza)
+    // }
+    var answer = Math.ceil(n/7)
+    return answer
+
+}
+// 피자는 7조각 사람은 n명
