fca2272d6f6519d69ef0c089f7a006134afe1723
TaekJinJang <93184838+TaekJinJang@users.noreply.github.com>
2022-11-30 22:57:42 +0900

    [level 0] Title: 배열 뒤집기, Time: 2.17 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120821. 배열 뒤집기/배열 뒤집기.js
@@ -0,0 +1,13 @@
+function solution(num_list) {
+    var answer = [];
+    let cnt = num_list.length
+    console.log(cnt)
+
+        for(j=cnt-1;j>=0;j--){
+            answer.push(num_list[j]) 
+
+
+    }
+    
+    return answer;
+}
