9e0b3c3dcba805ae51186adfba89d7c401882c5f
Taek_Jini <93184838+TaekJinJang@users.noreply.github.com>
2023-01-10 22:43:17 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.js
@@ -0,0 +1,12 @@
+function solution(my_string) {
+    var answer = 0;
+    for(i=0;i<my_string.length;i++){
+        const item = Number(my_string[i])
+        if(Number.isNaN(item)) {
+            continue;
+        }
+        answer +=item
+            
+    }
+    return answer;
+}
