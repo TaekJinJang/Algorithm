19a84831617ac4c2f5515072caeba0c00404d9d5
Taek_Jini <93184838+TaekJinJang@users.noreply.github.com>
2023-01-11 16:45:24 +0900

    [level 0] Title: 배열 원소의 길이, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120854. 배열 원소의 길이/배열 원소의 길이.js
@@ -0,0 +1,9 @@
+function solution(strlist) {
+    // var answer = [];
+    // for(i=0;i<strlist.length;i++){
+    //     const item = strlist[i];
+    //     answer.push(item.length)
+    // }
+    // return answer;
+    return strlist.map((str)=>str.length)
+}
