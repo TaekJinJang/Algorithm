d7bdf457b71314cbd4a8ae7778efc0e8c7d1497c
Taek_Jini <93184838+TaekJinJang@users.noreply.github.com>
2023-02-16 16:31:42 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -0,0 +1,16 @@
+function solution(num) {
+    var answer = 0;
+    let cnt = 0;
+    while (num > 1) {
+        if (num % 2 === 0) {
+            num = num/2
+            cnt ++;
+        } else if (num % 2 === 1) {
+            num = (num*3) +1
+            cnt ++
+        }
+        if (cnt === 500) return -1
+    }
+    answer = cnt
+    return answer;
+}
