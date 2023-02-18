a9b3377f9f6449e465e57bdc0858c0f55bc30fc6
Taek_Jini <93184838+TaekJinJang@users.noreply.github.com>
2023-02-18 14:49:26 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 13.34 ms, Memory: 83.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -0,0 +1,14 @@
+
+
+function solution(arr)
+{
+    var answer = [];
+    for (let i = 1; i < arr.length ; i++ ) {
+            if ( arr[i] !== arr[i-1]) {
+               answer.push(arr[i-1]) 
+            }
+    }
+    answer.push(arr[arr.length -1])
+    return answer;
+
+}
