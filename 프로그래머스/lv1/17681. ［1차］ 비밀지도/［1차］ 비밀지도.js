6f2328b076d5f310a4afa6e6f556a2f3d36ee4fb
Taek_Jini <93184838+TaekJinJang@users.noreply.github.com>
2023-02-22 18:25:08 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -0,0 +1,16 @@
+function solution(n, arr1, arr2) {
+    var answer = [];
+    let temp = []
+    let a = arr1.map((num)=>num.toString(2).padStart(n,0))
+    let b = arr2.map((num)=>num.toString(2).padStart(n,0))
+    for(let i=0;i<n;i++){
+        temp.push(String(+a[i] + +b[i]).padStart(n,0))
+    }
+    for(let x of temp) {
+        let temp = x.split('')
+        answer.push(temp.map((item)=>item =="0"?" ":"#").join(''))
+    }
+    
+
+    return answer;
+}
