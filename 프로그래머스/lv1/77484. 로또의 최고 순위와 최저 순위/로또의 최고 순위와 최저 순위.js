2b5f3479cb67bc269f9ec1614b8c11358235fdcc
Taek_Jini <93184838+TaekJinJang@users.noreply.github.com>
2023-03-05 15:48:32 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 5.08 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
@@ -0,0 +1,25 @@
+function solution(lottos, win_nums) {
+    var answer = [];
+    let num = 7
+    let zero = 0
+    lottos.sort()
+    win_nums.sort()
+    console.log(lottos)
+    console.log(win_nums)
+    for(let i=0;i<lottos.length;i++){
+        if(lottos[i] === 0) zero ++
+        for (let j=0;j<lottos.length;j++){
+            if(lottos[i] === win_nums[j]) {
+            num--
+            }
+        }
+    }
+    if( num == 7) num = 6
+    if(num == 0) num= 1
+    if(zero == 6) {
+        return [1,6]
+    }
+    answer.push(num-zero)
+    answer.push(num)
+    return answer;
+}
