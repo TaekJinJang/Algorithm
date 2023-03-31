130e966348a73b1eee8ed4778025fac6c3fdc772
Taek_Jini <93184838+TaekJinJang@users.noreply.github.com>
2023-03-31 14:14:49 +0900

    [unrated] Title: 덧칠하기, Time: 2.34 ms, Memory: 37.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.js
@@ -0,0 +1,14 @@
+function solution(n, m, section) {
+    let idx = section[0]
+    let result = 0
+    
+    section.forEach((e)=>{
+        if(e>=idx) {
+            idx = m + e
+            result +=1
+        }
+
+    })
+    
+    return result;
+}
