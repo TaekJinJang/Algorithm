3a25d4b6643b117d1e51820406a96eb3a2bd1348
Taek_Jini <93184838+TaekJinJang@users.noreply.github.com>
2023-04-12 10:04:33 +0900

    [level 2] Title: 카펫, Time: 0.06 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42842. 카펫/카펫.js
@@ -0,0 +1,22 @@
+function solution(brown, yellow) {
+    var answer = [];
+    let sum = brown + yellow
+    
+    for(let i=3;i <= sum / i;i++){ // 최소 w,h = 3
+        let w = sum / i
+        let h = i
+        if((w-2)*(h-2) === yellow) {
+            answer[0] = w, answer[1] = h
+        }
+    }
+    
+    return answer;
+}
+
+    
+// 2x + 2y - 4 = brown
+// (x - 2)(y - 2) = yellow
+// x = (brown / 2) + 2 - y;
+// (brown / 2 - y)(y - 2) = yellow
+// (y - brown / 2)(y - 2) = - yellow;
+// y ^2 - (2 + brown / 2)y + brown + yellow = 0;
