731b917060ac6c73623691a444c0a3b274d615de
Taek_Jini <93184838+TaekJinJang@users.noreply.github.com>
2023-04-14 22:03:48 +0900

    [unrated] Title: 광물 캐기, Time: 5.62 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/172927. 광물 캐기/광물 캐기.js
+++ 프로그래머스/unrated/172927. 광물 캐기/광물 캐기.js
@@ -5,6 +5,7 @@ function solution(picks, minerals) {
     const countedMinerals = slicedMinerals.reduce((acc, cur, idx) => {
         const index = Math.floor(idx / 5);
         if (!acc[index]) acc[index] = [0, 0, 0];
+        // 처음엔 빈배열이기때문에 0,0,0 을 지정해줌
         if (cur === 'diamond') acc[index][0]++;
         else if (cur === 'iron') acc[index][1]++;
         else if (cur === 'stone') acc[index][2]++;
@@ -12,6 +13,7 @@ function solution(picks, minerals) {
         return acc;
       }, []);
     
+    // 이중배열 sort (1순위 : diamond가 더 많은 순 , 2순위 : iron이 더 많은 순)
     const sortedMinerals = countedMinerals.sort((a,b) => b[0] - a[0] || b[1] - a[1])
     
     console.log(sortedMinerals)
@@ -21,15 +23,13 @@ function solution(picks, minerals) {
       answer += dia + iron + stone;
       picks[0]--;
     } else if (picks[1]) {
-      answer += dia * 5 + iron + stone;
+      answer += (dia * 5) + iron + stone;
       picks[1]--;
     } else if (picks[2]) {
-      answer += dia * 25 + iron * 5 + stone;
+      answer += (dia * 25) + (iron * 5) + stone;
       picks[2]--;
     }
   }
     
     return answer;
 }
-
-// 
