268adfebc903c44d98625cee408651674bbc178f
Taek_Jini <93184838+TaekJinJang@users.noreply.github.com>
2023-04-17 00:24:43 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.03 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12980. 점프와 순간 이동/점프와 순간 이동.js
@@ -0,0 +1,15 @@
+function solution(n)
+{
+    let answer = 0
+   while(n !== 0) {
+       if(n % 2 === 1) {
+           n--;
+           answer++;
+       } else{
+           n /=2
+       }
+       
+   }
+    return answer;
+}
+
