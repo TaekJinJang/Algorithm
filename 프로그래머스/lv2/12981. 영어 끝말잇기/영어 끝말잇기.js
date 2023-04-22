c70adb0d8de57cf353ed1dd72717598d48ef33b9
Taek_Jini <93184838+TaekJinJang@users.noreply.github.com>
2023-04-22 17:07:03 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.21 ms, Memory: 33.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.js
+++ 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.js
@@ -1,34 +1,23 @@
 function solution(n, words) {
     var answer = [0,0];
     let temp = [];
-    
     for(let i=0;i<words.length;i++){
         if(temp.length == 0){ 
             // 길이가 0이 아닐때 끝 스펠링과 첫 스펠링이 맞는지
             temp.push(words[i])
             continue;
         }
-        if(words[i].length < 2) {
-            let number = i % n + 1;
-            let repeat = Math.ceil((i + 1) / n);
-            answer = [number,repeat]
-            return answer
-        }
-        if(temp[temp.length-1].slice(-1) !== words[i].slice(0,1)){
+        
+
+        
+        if(words[i].length < 2 || temp[temp.length-1].slice(-1) !== words[i].slice(0,1) || temp.includes(words[i]) === true) {
             // n % i 해서 나머지값이 순서
             let number = i % n + 1;
             let repeat = Math.ceil((i + 1) / n);
             answer = [number,repeat]
             return answer
         }
-
-        if(temp.includes(words[i]) === true){
-
-            let number = i % n + 1;
-            let repeat = Math.ceil((i + 1) / n);
-            answer = [number,repeat]
-            return answer
-            } 
+    
         temp.push(words[i])
     }
 
