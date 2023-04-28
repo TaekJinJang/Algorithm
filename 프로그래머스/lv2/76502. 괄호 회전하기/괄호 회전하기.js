7432fe02355fb1ca0224d0f3c33346f7d08dead1
Taek_Jini <93184838+TaekJinJang@users.noreply.github.com>
2023-04-28 18:39:56 +0900

    [level 2] Title: 괄호 회전하기, Time: 0.08 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.js
@@ -0,0 +1,19 @@
+function solution(s) {
+    let answer = 0;
+    let string = [...s]; 
+
+    for (let i = 0; i < s.length; i++) {
+        const stack = [];
+        for (const x of string) {
+            const top = stack[stack.length-1];
+            if (top === '(' && x === ')') { stack.pop(); continue; }
+            if (top === '[' && x === ']') { stack.pop(); continue; }
+            if (top === '{' && x === '}') { stack.pop(); continue; }
+            stack.push(x);
+        }
+        if (stack.length === 0) answer++;
+        string.push(string.shift()); // 문자열을 왼쪽으로 돌리기 
+//         [
+    }
+    return answer;
+}
