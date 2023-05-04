abc33360b8fbb8a365c1c9ea87bde8e11ab8589d
Taek_Jini <93184838+TaekJinJang@users.noreply.github.com>
2023-05-04 19:15:35 +0900

    [level 2] Title: 오픈채팅방, Time: 163.03 ms, Memory: 98.8 MB -BaekjoonHub

--- 프로그래머스/lv2/42888. 오픈채팅방/오픈채팅방.js
+++ 프로그래머스/lv2/42888. 오픈채팅방/오픈채팅방.js
@@ -1,25 +1,23 @@
 function solution(record) {
-    var answer = []
-
-    let data = {}
+    let answer = []
+    const user = {}
+    const command = []; 
     for(let i=0;i <record.length;i++){
-        let message = record[i].split(' ')
-        if(message[0] === 'Enter' || message[0] === 'Change') {
-            data[message[1]] = message[2]
+        
+        const [cmd, id, nickname] = record[i].split(' ')
+        
+        if(nickname) user[id] = nickname
+        if(cmd !== 'Change') {
+            command.push([cmd,id])
         }
+        
     }
-    for(let i=0;i<record.length;i++){
-        let message = record[i].split(' ')
-        if(message[0] === 'Enter') {
-            answer.push(`${data[message[1]]}님이 들어왔습니다.`)
-        }
-        if(message[0] === 'Leave') {
-            answer.push(`${data[message[1]]}님이 나갔습니다.`)
-        }
-    }
+    
+    answer = command.map(([cmd,id]) => {
+        return `${user[id]}님이 ${cmd === 'Enter' ? '들어왔습니다.' : '나갔습니다.'}`
+    })
+
+
     return answer;
     
 }
-
-// for문으로 record.length 만큼 돌려서
-// split('') 으로 나눠주고 두번째 꺼를 객체 key , 닉네임을 value, 첫번째꺼는 if문으로 판별
