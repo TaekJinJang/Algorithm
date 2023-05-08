7753f78879a1376efc3673dd781b0bcd5fcb5954
Taek_Jini <93184838+TaekJinJang@users.noreply.github.com>
2023-05-08 16:04:24 +0900

    [level 2] Title: 피로도, Time: 0.20 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/87946. 피로도/피로도.js
@@ -0,0 +1,18 @@
+function solution(k, dungeons) {
+    let answer = 0;
+    const visited = Array(dungeons.length).fill(false)
+    
+    const dfs = (k, count) => {
+        for(let i=0;i<dungeons.length;i++) {
+            if(!visited[i] && dungeons[i][0] <= k) {
+                visited[i] = true;
+                dfs(k-dungeons[i][1], count+1);
+                visited[i] = false
+            }
+        }
+        answer = Math.max(answer,count)
+    }
+    dfs(k,0)
+    
+    return answer;
+}
