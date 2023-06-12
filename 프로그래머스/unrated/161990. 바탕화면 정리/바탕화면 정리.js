1a1c8c1ac20a8453d95d0ddc1b20c50c690caca4
Taek_Jini <93184838+TaekJinJang@users.noreply.github.com>
2023-06-13 04:23:39 +0900

    [unrated] Title: 바탕화면 정리, Time: 0.25 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/161990. 바탕화면 정리/바탕화면 정리.js
+++ 프로그래머스/unrated/161990. 바탕화면 정리/바탕화면 정리.js
@@ -2,20 +2,15 @@ function solution(wallpaper) {
 
   const x = [];
   const y = [];
-
-  for (let i = 0; i < wallpaper.length; i++) {
-    for (let j = 0; j < wallpaper[i].length; j++) {
-      if (wallpaper[i][j] === "#") {
-        x.push(i);
-        y.push(j);
-      }
+    
+    for(let i=0;i<wallpaper.length;i++){
+        for(let j=0;j<wallpaper[i].length;j++){
+            if(wallpaper[i][j] === '#') {
+                x.push(i);
+                y.push(j);
+            }
+        }
     }
-  }
-
-
-  x.sort((a, b) => a - b);
-  y.sort((a, b) => a - b);
-
-
-  return [x[0], y[0], x[x.length - 1] + 1, y[y.length - 1] + 1];
+    
+    return [Math.min(...x),Math.min(...y),Math.max(...x)+1,Math.max(...y)+1]
 }
