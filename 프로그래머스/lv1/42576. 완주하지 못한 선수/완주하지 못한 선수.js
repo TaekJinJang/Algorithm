faef4983dc6e4499b939ed73b8bf4076b8165d3d
Taek_Jini <93184838+TaekJinJang@users.noreply.github.com>
2023-06-13 23:49:19 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 37.69 ms, Memory: 67.2 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,14 +1,14 @@
 function solution(participant, completion) {
-    const n = participant.length;
-    let answer = '';
+    let answer = new Map();
+
+    completion.forEach(c=>{
+        if(answer.has(c)) answer.set(c,answer.get(c)+1)
+        else answer.set(c,1)
+    })
     
-    participant.sort();
-    completion.sort();
-    
-    for (let i = 0; i < n; i++){
-        if (participant[i] !== completion[i]) {
-            answer = participant[i];
-            return answer;
-        }
+    for(const p of participant) {
+        if(!answer.has(p) || answer.get(p) === 0) return p
+        else answer.set(p,answer.get(p)-1)
     }
+
 }
