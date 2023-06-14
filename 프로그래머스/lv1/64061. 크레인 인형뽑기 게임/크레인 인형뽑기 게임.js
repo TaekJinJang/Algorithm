1ef7c3aaf5b99c44a9c2a426a9b7870316fc4794
Taek_Jini <93184838+TaekJinJang@users.noreply.github.com>
2023-06-15 01:08:17 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 0.76 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/lv1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
+++ 프로그래머스/lv1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
@@ -1,30 +1,23 @@
 function solution(board, moves) {
-    var answer = 0;
-    let item = 0
-    let stack = []
-    
-    for(let i=0;i<moves.length;i++){
-        let box = moves[i]-1    
-        
-        for(let j=0;j<board.length;j++){
-            const doll = Number(board[j].slice(box,box+1))
-            // doll 을 board[j][box] 로 바꿀 수 있음
-            if(doll !== 0)  {
-                stack.push(doll)
-                board[j].splice(box,1,0)
-                // board[j][box] = 0 으로 바꿀 수 있음
-                
-                if(stack[stack.length-1] === stack[stack.length-2]) {
-                    stack.splice(stack.length-2,2)
-                    answer +=2
-                }
-                
-                break;
-            }
- 
+  let result = 0;
+  const stack = [];
+
+  for (const move of moves) {
+    const curLocation = move - 1;
+
+    for (const row of board) {
+      if (row[curLocation] !== 0) {
+        if (stack.at(-1) === row[curLocation]) {
+          stack.pop();
+          result += 2;
+        } else {
+          stack.push(row[curLocation]);
         }
+        row[curLocation] = 0;
+        break;
+      }
     }
-    return answer;
+  }
+
+  return result;
 }
-// 2중for문으로 배열안에 moves(i) 값이 있다면 빼서 바구니 넣음 
-// 둘이 같다면 ? 둘다 pop  result++
