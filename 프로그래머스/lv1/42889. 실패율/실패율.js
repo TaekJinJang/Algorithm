3a53fa70cbd72c739559ab597b995ea92a40a7ea
Taek_Jini <93184838+TaekJinJang@users.noreply.github.com>
2023-06-16 17:27:54 +0900

    [level 1] Title: 실패율, Time: 0.09 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/42889. 실패율/실패율.js
+++ 프로그래머스/lv1/42889. 실패율/실패율.js
@@ -1,26 +1,19 @@
 function solution(N, stages) {
     let arr = [];
-    let totalNum = stages.length;
-    for (let i=1; i<=N; i++){
-        // 현재 단계에서 실패한 유저수를 계산
-        // 현재까지의 누적 도전자수를 나누어 실패율 계산
-        // 현재까지의 누적 도전자수에 실패한 유저수 빼기
-        // 
-        let stageNum = stages.filter(ele=> ele==i).length
-        let failRatio = 0;
-        if (stageNum===0){
-            failRatio = 0;
-        }else{
-            failRatio = (stageNum)/totalNum;
-        }
-        totalNum -= stageNum;
-        arr.push({idx:i,ratio:failRatio});
+
+    let failUsers = 0;
+    for(let i=1;i<=N;i++){
+        const failUser = stages.filter(stage=>stage === i).length
+        
+        let failRate = (failUser === 0) ? 0 : failUser / (stages.length - failUsers)
+        failUsers += failUser;
+        arr.push([failRate,i])
     }
-    // 실패율 내림차순으로 정렬하고 실패율이 같다면 stage 오름차순으로 정렬
+    
     arr.sort((a,b)=>{
-        if(a.ratio>b.ratio) return -1;
-        if(a.ratio<b.ratio)return 1;
-        return a.idx - b.idx
+        return b[0] - a[0];
     })
-    return arr.map(ele=>ele.idx);
+    
+    
+    return arr.map(([rate,idx])=>idx);
 }
