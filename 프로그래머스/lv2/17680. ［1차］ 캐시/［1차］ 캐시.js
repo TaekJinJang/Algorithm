98e88dc0e621bbf9eb7d66ed8dda8dd3f0edcbb3
Taek_Jini <93184838+TaekJinJang@users.noreply.github.com>
2023-06-18 03:29:41 +0900

    [level 2] Title: [1차] 캐시, Time: 0.55 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/17680. ［1차］ 캐시/［1차］ 캐시.js
+++ 프로그래머스/lv2/17680. ［1차］ 캐시/［1차］ 캐시.js
@@ -1,28 +1,27 @@
 function solution(cacheSize, cities) {
-    
+    // 3: 05
+    // 캐시 안에 도시 이름이 있다면 hit
+    // hit면 indexOf로 찾아서 삭제 후 다시 push
+    // 없으면 shift 후 새로운거 push
     let answer = 0;
-    const cache = []
-    
-    if(cacheSize === 0) return cities.length*5
-    
-    for(let i=0;i<cities.length;i++){
-        const item = cities[i].toLowerCase()
-        answer += cache.includes(item) ? 1 : 5
-        if(cache.length < cacheSize) {
-            cache.push(item)
-
-            continue;
-        }
-        if(cache.includes(item)) {
-            cache.splice(cache.indexOf(item),1)
-            cache.push(item)
-
+    let cache = [];
+    cities.forEach(city=>{
+        city = city.toUpperCase();
+        if(cache.includes(city)) {
+            answer ++;
+            const idx = cache.indexOf(city)
+            cache.splice(idx,1)
+            cache.push(city)
         } else {
-            cache.shift()
-            cache.push(item)
-
+            if(cache.length === cacheSize) cache.shift();
+            answer += 5;
+            if(cacheSize > 0) cache.push(city)
         }
-    }
+            
+    } )
+    
+    
+    
 
     return answer;
 }
