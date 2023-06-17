9f542196e330ce38f4e96afee0d357a0de176a1d
Taek_Jini <93184838+TaekJinJang@users.noreply.github.com>
2023-06-18 05:18:38 +0900

    [unrated] Title: 대충 만든 자판, Time: 0.99 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/unrated/160586. 대충 만든 자판/대충 만든 자판.js
+++ 프로그래머스/unrated/160586. 대충 만든 자판/대충 만든 자판.js
@@ -1,19 +1,21 @@
 function solution(keymap, targets) {
     var answer = [];
-    const key = keymap.reduce((key,keymap) => {
-        [...keymap].forEach((k,i) => {
-            key[k] = Math.min(key[k] || i+1, i +1)       
+    let keyBtn = keymap.reduce((key,map)=> {
+        [...map].forEach((m,i)=>{
+            key[m] = Math.min(key[m] || i+1,i+1)
         })
         return key
-    },{});
+    },{})
     
-    [...targets].forEach((target) => {
+    targets.forEach(target=>{
         let count = 0;
-        [...target].forEach((t) => {
-            count += key[t]
+        [...target].forEach(t=>{
+            count += keyBtn[t]
         })
+        
         answer.push(count || -1)
     })
+    
     
     return answer;
 }
