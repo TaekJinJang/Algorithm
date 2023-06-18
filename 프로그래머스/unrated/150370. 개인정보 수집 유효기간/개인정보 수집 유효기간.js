838294759afbdcd4bc4a9393752b61d68421c985
Taek_Jini <93184838+TaekJinJang@users.noreply.github.com>
2023-06-18 20:12:56 +0900

    [unrated] Title: 개인정보 수집 유효기간, Time: 1.11 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
+++ 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
@@ -1,25 +1,27 @@
+
 function solution(today, terms, privacies) {
-    var answer = [];
-    const [todayYear,todayMonth,todayDay] = today.split('.').map((t)=>Number(t))
-    const todayDate = (todayYear*12*28) + (todayMonth*28) + todayDay
-    const term = terms.reduce((acc,cur) => {
-        const [type,period] = cur.split(' ')
-        acc[type] = parseInt(period)
+    const dateCalc = ([y,m,d]) => {
+        const [todayY,todayM,todayD] = today.split('.')
+        const year = (todayY - y) * 12;
+        const month = todayM-m;
+        const day = (todayD - d < 0) ? -1 : 0;
+        return year+month+day;
+    }
+    
+    let answer = [];
+    const period = terms.reduce((acc,cur)=>{ // 기간을 Object로 변경
+        const [term,month] = cur.split(' ');
+        acc[term] = Number(month)
         return acc
-    },{})
-
-    privacies.forEach((p,i) => {
-        let [expireDate,type] = p.split(' ')
-        let [year, month, day] = expireDate.split('.').map((e)=>Number(e))
-        
-        const period = term[type]
-        const date = (year*12*28) + ((month+period)*28) + day
-        if(todayDate >= date ) answer.push(i+1)
-        
+    },{}) 
+    
+    privacies.forEach((p,i)=> {
+        const [date,term] = p.split(' ');
+        const [year,month,day] = date.split('.');
+        if(dateCalc([year,month,day]) >= period[term]) {
+            answer.push(i+1)
+        }
     })
-
-    
-    
     return answer;
 }
 
